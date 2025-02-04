709ce8f9927506169d29b51fe7e22dcf0fd56b52
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-02-05 02:54:39 +0600

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,25 @@
 import mongoose from 'mongoose';
 
 let isConnected = false; //track the connection status
+
+export const connectToDatabase = async () => {
+    mongoose.set('strictQuery', true);
+    //sets the mongoose options
+
+    if (isConnected) {
+        console.log('MongoDb is already connected!');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+        })
+        isConnected = true;
+
+        console.log('MongoDb connected!');
+    } catch (error) {
+        console.log(error);
+    }
+}
