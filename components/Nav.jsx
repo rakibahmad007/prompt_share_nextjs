c531193b953e43b9dfe4390e6ab15e6e69b7756a
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-02-05 01:16:40 +0600

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,20 +3,137 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn, signOut, useSession, getProviders } from "next-auth/client";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  useEffect(() => {
+
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, []);
+
   return (
-    <nav className="flex-betweem w-full mb-16 pt-3">
+    <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/logo.png" width={50} height={50} alt="logo" />
-        <h1 className="text-2xl">PromptShare</h1>
-        <Image 
-        src="/assets/images/logo.png" 
-        alt="logo" 
-        width={50} 
-        height={50} 
+        <Image
+          src="/assets/images/logo.svg"
+          alt="PromptShare"
+          width={30}
+          height={30}
+          className="object-contain"
         />
+        <p className="logo_text">PromptShare</p>
       </Link>
+
+      {/* Desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+
+               Create Prompt
+
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image 
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="Profile"
+              />
+            </Link>
+          
+           </div>
+          ) : (<>
+              {providers && 
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn">
+                    Sign In
+                  </button>
+              ))}
+          </>)}
+      </div>
+
+      {/* Mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="Profile"
+              onClick={() => setToggleDropDown((prev)=> !prev)}
+
+              // in react for this toggleDropDown, we cannot just set toggleDropDown to true/false and then set the handler to the opposite of the current value. This will create issues with the state not updating properly. Instead, we need to use the previous value of the state to determine the new value. This is why we use the prev parameter in the setToggleDropDown function.
+            />
+
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+              {providers && 
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn">
+                    Sign In
+                  </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
